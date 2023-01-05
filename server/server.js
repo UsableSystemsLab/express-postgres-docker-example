ba4f01fdab69812e7483ab8572cdbe80b1223f64
Khalid Alharbi <kalharbi@users.noreply.github.com>
2023-01-05 09:06:41 +0300

    server setup

--- server/server.js
+++ server/server.js
@@ -12,13 +12,21 @@ const HOST = process.env.EXPRESS_HOST;
 const app = express();
 app.use(cors())
 
-// HTTP POST requests
-app.post("/", (req, res) => {
-    console.log("I got this ", req.body)
+// HTTP POST: store the short code for the long url
+// HTTP POST http://host:port/save body -> https://github.com => gdfte545 => store in postgresdb => respond with short code
+app.post("/save", (req, res) => {
+    console.log("I got this ", req.body);
+    res.send(nanoid.nanoid());
 })
-// HTTP GET requests
-app.get('/', (req, res) => {
+// HTTP GET: get the long url by the short code
+// HTTP GET http://host:port/get?code=gdfte545 => postgres => return to longurl => client will HTTP redirect
+app.get('/get', (req, res) => {
   res.send('Hello World');
+});
+
+// HTTP GET requests
+app.get('/health', (req, res) => {
+  res.status(200).send('healthy');
 });
 
 app.listen(PORT, HOST, () => {
